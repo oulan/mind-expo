f5e07482145cb38f523ad2cbead45ef7724d46c2
Ou Lanhui <o@iclue.net>
2012-05-04 09:47:44 +0800

    Open source for Enterprise and pad software preparing

---
+++ docs/pad.mm
@@ -0,0 +1,11 @@
+<map version="1.0.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1336089763385" ID="ID_1842683013" MODIFIED="1336089768151" TEXT="pad">
+<node CREATED="1336090548881" ID="ID_1546799747" MODIFIED="1336090558924" POSITION="right" TEXT="remote">
+<node CREATED="1336090563710" ID="ID_1992246767" LINK="http://code.google.com/p/android-xbmcremote/" MODIFIED="1336090575303" TEXT="Official XBMS Remote for Android"/>
+</node>
+<node CREATED="1336090629557" ID="ID_604716833" MODIFIED="1336090632643" POSITION="left" TEXT="messaging">
+<node CREATED="1336090634317" ID="ID_1703435955" LINK="https://developers.google.com/android/c2dm/" MODIFIED="1336090641707" TEXT="Android Cloud to Device Messaging Framework"/>
+</node>
+</node>
+</map>
